d4d612f52559946a75c0ec1dafd90f2b04512e09
t-nakamura <im9today@me.com>
2021-10-10 17:05:16 +0900

    font display swap

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,7 +17,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="http://fonts.googleapis.com/css?family=Orbitron"
+            href="https://fonts.googleapis.com/css?family=Orbitron&display=swap"
             rel="stylesheet"
           />
         </Head>
