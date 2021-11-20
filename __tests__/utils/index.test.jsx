d5e7aa995559664af2aec480151f614f0a0d9a96
t-nakamura <im9today@me.com>
2021-11-20 09:50:05 +0900

    unit test

---
+++ __tests__/utils/index.test.jsx
@@ -0,0 +1,15 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Home from "../../pages/index";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+
+    expect(screen.getByRole("heading")).toBeTruthy();
+  });
+});
