46f355141625b83ba014b1ebf95d0447cc420a18
t-nakamura <im9today@me.com>
2022-12-30 23:31:39 +0900

    update next.js 13

--- __tests__/utils/index.test.jsx
+++ __tests__/utils/index.test.jsx
@@ -6,6 +6,10 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Home from "../../pages/index";
 
+jest.mock("next/router", () => ({
+  useRouter: jest.fn(),
+}));
+
 describe("Home", () => {
   it("renders a index Page", () => {
     render(<Home />);
